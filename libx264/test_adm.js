4f3257881374b277eb6bad6d6e4b848dbcb9396c
mulder2 <mulder2@gmx.de>
2010-05-17 19:53:18 +0000

    [libx264] Update build script and patch collection

--- libx264/test_adm.js
+++ libx264/test_adm.js
@@ -8,9 +8,9 @@ app.load("E:/Samples/Lossless/foreman_cif.352x288.avi");
 app.append("E:/Samples/Lossless/bus_cif.352x288.avi");
 app.append("E:/Samples/Lossless/football_cif.352x288.avi");
 app.append("E:/Samples/Lossless/silent_cif.352x288.avi");
+app.append("E:/Samples/Lossless/stefan_cif.352x288.avi");
 
 /*
-app.append("E:/Samples/Lossless/stefan_cif.352x288.avi");
 app.append("E:/Samples/Lossless/firebelly.352x288.avi");
 */
 
