9a28a51c26f29a67e2e30a875650d15da32631c9
mulder2@gmx.de <mulder2@gmx.de@f9abe1ba-e1ba-11de-9097-f71ab900b11a>
2010-10-31 12:40:33 +0000

    [LameXP Qt] Initial commit of LameXP Qt rewrite

---
+++ trunk/LameXP_QT/src/Global.h
@@ -0,0 +1,49 @@
+///////////////////////////////////////////////////////////////////////////////
+// LameXP - Audio Encoder Front-End
+// Copyright (C) 2004-2010 LoRd_MuldeR <MuldeR2@GMX.de>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program; if not, write to the Free Software Foundation, Inc.,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+// http://www.gnu.org/licenses/gpl-2.0.txt
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+//MSVC
+#include "Targetver.h"
+
+//Stdlib
+#include <stdio.h>
+#include <tchar.h>
+
+//Win32
+#include <Windows.h>
+
+//LameXP version info
+unsigned int lamexp_version_major(void);
+unsigned int lamexp_version_minor(void);
+unsigned int lamexp_version_build(void);
+const char *lamexp_version_date(void);
+const char *lamexp_version_release(void);
+
+//Helper macros
+#define LAMEXP_DELETE(PTR) if(PTR) { delete PTR; PTR = NULL; }
+
+//Check for debug build
+#if defined(_DEBUG) || defined(QT_DEBUG)
+#define LAMEXP_CHECK_DEBUG_BUILD printf("\nDEBUG BUILD: DO NOT RELEASE THIS BINARY TO THE PUBLIC !!!\n\n")
+#else
+#define LAMEXP_CHECK_DEBUG_BUILD printf("\n")
+#endif
