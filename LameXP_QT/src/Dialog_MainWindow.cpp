9df59b696241e1e4c2c395c2fbb458393a2c9b37
mulder2 <mulder2@gmx.de>
2011-02-11 19:17:42 +0000

    Sync with GIT:
    Revision: 54ae153b54c5224f2445586f6549e71847c219ed
    Author: lordmulder
    Date: 10.02.2011 23:20:30
    Message:
    Updated Spanish translation. Thanks to Rub3nCT <Rub3nCT@gmail.com>.
    
    ----
    Modified : etc/Translation/LameXP_ES.ts
    Modified : res/localization/LameXP_ES.qm
    Modified : src/Config.h
    
    Revision: 476265df3af0b82f8e5fef96b0ad0982e6fb04e8
    Author: lordmulder
    Date: 10.02.2011 16:08:03
    Message:
    Save the "Position" field on the "Meta Data" tab.
    
    ----
    Modified : gui/LogViewDialog.ui
    Modified : gui/MetaInfo.ui
    Modified : gui/ProcessingDialog.ui
    Modified : gui/UpdateDialog.ui
    Modified : src/Config.h
    Modified : src/Dialog_About.cpp
    Modified : src/Dialog_MainWindow.cpp
    Modified : src/Model_MetaInfo.cpp
    Modified : src/Model_MetaInfo.h
    Modified : src/Model_Settings.cpp
    Modified : src/Model_Settings.h
    
    Revision: cfb20fe3cbfb3306eaa99397d27f36f09513bea1
    Author: lordmulder
    Date: 09.02.2011 23:36:17
    Message:
    Added support for custom encoder parameters.
    
    ----
    Modified : etc/Translation/Blank.ts
    Modified : etc/Translation/LameXP_DE.ts
    Modified : etc/Translation/LameXP_ES.ts
    Modified : etc/Translation/LameXP_FR.ts
    Modified : etc/Translation/LameXP_IT.ts
    Modified : gui/MainWindow.ui
    Modified : res/localization/LameXP_DE.qm
    Modified : src/Config.h
    Modified : src/Dialog_MainWindow.cpp
    Modified : src/Dialog_MainWindow.h
    Modified : src/Dialog_Processing.cpp
    Modified : src/Encoder_AAC.cpp
    Modified : src/Encoder_Abstract.cpp
    Modified : src/Encoder_Abstract.h
    Modified : src/Encoder_FLAC.cpp
    Modified : src/Encoder_MP3.cpp
    Modified : src/Encoder_Vorbis.cpp
    Modified : src/Model_Settings.cpp
    Modified : src/Model_Settings.h
    
    Revision: fb77806ada9db175f81eac0884ad0fdbb94a41c4
    Author: lordmulder
    Date: 07.02.2011 22:07:24
    Message:
    Removed installer workaround for a bug of the LockedList plug-in on Win2k, as the bug has finally been fixed.
    
    ----
    Modified : etc/NSIS/setup.nsi

--- LameXP_QT/src/Dialog_MainWindow.cpp
+++ LameXP_QT/src/Dialog_MainWindow.cpp
@@ -168,6 +168,7 @@ MainWindow::MainWindow(FileListModel *fileListModel, AudioFileModel *metaInfo, S
 	//Setup "Meta Data" tab
 	m_metaInfoModel = new MetaInfoModel(m_metaData, 6);
 	m_metaInfoModel->clearData();
+	m_metaInfoModel->setData(m_metaInfoModel->index(4, 1), m_settings->metaInfoPosition());
 	metaDataView->setModel(m_metaInfoModel);
 	metaDataView->verticalHeader()->setResizeMode(QHeaderView::ResizeToContents);
 	metaDataView->verticalHeader()->hide();
@@ -217,6 +218,10 @@ MainWindow::MainWindow(FileListModel *fileListModel, AudioFileModel *metaInfo, S
 	while(checkBoxBitrateManagement->isChecked() != m_settings->bitrateManagementEnabled()) checkBoxBitrateManagement->click();
 	while(checkBoxNeroAAC2PassMode->isChecked() != m_settings->neroAACEnable2Pass()) checkBoxNeroAAC2PassMode->click();
 	while(checkBoxNormalizationFilter->isChecked() != m_settings->normalizationFilterEnabled()) checkBoxNormalizationFilter->click();
+	lineEditCustomParamLAME->setText(m_settings->customParametersLAME());
+	lineEditCustomParamOggEnc->setText(m_settings->customParametersOggEnc());
+	lineEditCustomParamNeroAAC->setText(m_settings->customParametersNeroAAC());
+	lineEditCustomParamFLAC->setText(m_settings->customParametersFLAC());
 	connect(sliderLameAlgoQuality, SIGNAL(valueChanged(int)), this, SLOT(updateLameAlgoQuality(int)));
 	connect(checkBoxBitrateManagement, SIGNAL(clicked(bool)), this, SLOT(bitrateManagementEnabledChanged(bool)));
 	connect(spinBoxBitrateManagementMin, SIGNAL(valueChanged(int)), this, SLOT(bitrateManagementMinChanged(int)));
@@ -230,10 +235,15 @@ MainWindow::MainWindow(FileListModel *fileListModel, AudioFileModel *metaInfo, S
 	connect(spinBoxToneAdjustBass, SIGNAL(valueChanged(double)), this, SLOT(toneAdjustBassChanged(double)));
 	connect(spinBoxToneAdjustTreble, SIGNAL(valueChanged(double)), this, SLOT(toneAdjustTrebleChanged(double)));
 	connect(buttonToneAdjustReset, SIGNAL(clicked()), this, SLOT(toneAdjustTrebleReset()));
+	connect(lineEditCustomParamLAME, SIGNAL(editingFinished()), this, SLOT(customParamsChanged()));
+	connect(lineEditCustomParamOggEnc, SIGNAL(editingFinished()), this, SLOT(customParamsChanged()));
+	connect(lineEditCustomParamNeroAAC, SIGNAL(editingFinished()), this, SLOT(customParamsChanged()));
+	connect(lineEditCustomParamFLAC, SIGNAL(editingFinished()), this, SLOT(customParamsChanged()));
 	connect(buttonResetAdvancedOptions, SIGNAL(clicked()), this, SLOT(resetAdvancedOptionsButtonClicked()));
 	updateLameAlgoQuality(sliderLameAlgoQuality->value());
 	toneAdjustTrebleChanged(spinBoxToneAdjustTreble->value());
 	toneAdjustBassChanged(spinBoxToneAdjustBass->value());
+	customParamsChanged();
 	
 	//Activate file menu actions
 	connect(actionOpenFolder, SIGNAL(triggered()), this, SLOT(openFolderActionActivated()));
@@ -565,6 +575,7 @@ void MainWindow::changeEvent(QEvent *e)
 
 		//Force GUI update
 		m_metaInfoModel->clearData();
+		m_metaInfoModel->setData(m_metaInfoModel->index(4, 1), m_settings->metaInfoPosition());
 		updateEncoder(m_settings->compressionEncoder());
 		updateLameAlgoQuality(sliderLameAlgoQuality->value());
 
@@ -1397,7 +1408,14 @@ void MainWindow::makeFolderButtonClicked(void)
 void MainWindow::editMetaButtonClicked(void)
 {
 	ABORT_IF_BUSY;
-	m_metaInfoModel->editItem(metaDataView->currentIndex(), this);
+	
+	const QModelIndex index = metaDataView->currentIndex();
+	m_metaInfoModel->editItem(index, this);
+	
+	if(index.row() == 4)
+	{
+		m_settings->metaInfoPosition(m_metaData->filePosition());
+	}
 }
 
 /*
@@ -1822,7 +1840,7 @@ void MainWindow::toneAdjustTrebleChanged(double value)
 /*
  * Tone adjustment has been reset
  */
-void MainWindow::toneAdjustTrebleReset()
+void MainWindow::toneAdjustTrebleReset(void)
 {
 	spinBoxToneAdjustBass->setValue(m_settings->toneAdjustBassDefault());
 	spinBoxToneAdjustTreble->setValue(m_settings->toneAdjustTrebleDefault());
@@ -1831,9 +1849,35 @@ void MainWindow::toneAdjustTrebleReset()
 }
 
 /*
+ * Custom encoder parameters changed
+ */
+void MainWindow::customParamsChanged(void)
+{
+	lineEditCustomParamLAME->setText(lineEditCustomParamLAME->text().simplified());
+	lineEditCustomParamOggEnc->setText(lineEditCustomParamOggEnc->text().simplified());
+	lineEditCustomParamNeroAAC->setText(lineEditCustomParamNeroAAC->text().simplified());
+	lineEditCustomParamFLAC->setText(lineEditCustomParamFLAC->text().simplified());
+
+	bool customParamsUsed = false;
+	if(!lineEditCustomParamLAME->text().isEmpty()) customParamsUsed = true;
+	if(!lineEditCustomParamOggEnc->text().isEmpty()) customParamsUsed = true;
+	if(!lineEditCustomParamNeroAAC->text().isEmpty()) customParamsUsed = true;
+	if(!lineEditCustomParamFLAC->text().isEmpty()) customParamsUsed = true;
+
+	labelCustomParamsIcon->setVisible(customParamsUsed);
+	labelCustomParamsText->setVisible(customParamsUsed);
+	labelCustomParamsSpacer->setVisible(customParamsUsed);
+
+	m_settings->customParametersLAME(lineEditCustomParamLAME->text());
+	m_settings->customParametersOggEnc(lineEditCustomParamOggEnc->text());
+	m_settings->customParametersNeroAAC(lineEditCustomParamNeroAAC->text());
+	m_settings->customParametersFLAC(lineEditCustomParamFLAC->text());
+}
+
+/*
  * Reset all advanced options to their defaults
  */
-void MainWindow::resetAdvancedOptionsButtonClicked()
+void MainWindow::resetAdvancedOptionsButtonClicked(void)
 {
 	sliderLameAlgoQuality->setValue(m_settings->lameAlgoQualityDefault());
 	spinBoxBitrateManagementMin->setValue(m_settings->bitrateManagementMinRateDefault());
@@ -1847,6 +1891,11 @@ void MainWindow::resetAdvancedOptionsButtonClicked()
 	while(checkBoxBitrateManagement->isChecked() != m_settings->bitrateManagementEnabledDefault()) checkBoxBitrateManagement->click();
 	while(checkBoxNeroAAC2PassMode->isChecked() != m_settings->neroAACEnable2PassDefault()) checkBoxNeroAAC2PassMode->click();
 	while(checkBoxNormalizationFilter->isChecked() != m_settings->normalizationFilterEnabledDefault()) checkBoxNormalizationFilter->click();
+	lineEditCustomParamLAME->setText(m_settings->customParametersLAMEDefault());
+	lineEditCustomParamOggEnc->setText(m_settings->customParametersOggEncDefault());
+	lineEditCustomParamNeroAAC->setText(m_settings->customParametersNeroAACDefault());
+	lineEditCustomParamFLAC->setText(m_settings->customParametersFLACDefault());
+	customParamsChanged();
 	scrollArea->verticalScrollBar()->setValue(0);
 }
 
