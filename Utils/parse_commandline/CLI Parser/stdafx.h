1e9091ade72caf4cf3a29f21a2d04d9cfa4afd71
mulder2 <mulder2@gmx.de>
2012-06-26 15:54:32 +0000

    [parse_commandline] Committed initial version.

---
+++ Utils/parse_commandline/CLI Parser/stdafx.h
@@ -0,0 +1,20 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define _CRT_SECURE_NO_WARNINGS
+#define WIN32_LEAN_AND_MEAN
+
+#include <stdio.h>
+#include <tchar.h>
+#include <string.h>
+#include <Windows.h>
+#include <ShellAPI.h>
+#include <vector>
+
+// TODO: reference additional headers your program requires here
